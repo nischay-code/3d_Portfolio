2f258130e5985381264503aa6268ce4d33ce1bdb
Nischay Patel <95124327+nischay-code@users.noreply.github.com>
2023-11-25 11:22:47 +0530

    CTA

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,7 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+import CTA from "../components/CTA";
 
 const About = () => {
   return (
@@ -92,6 +93,8 @@ const About = () => {
           </VerticalTimeline>
         </div>
       </div>
+      <hr className="border-slate-200" />
+      <CTA />
     </section>
   );
 };
